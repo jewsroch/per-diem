ff2b9467e11550384aa77a097cfe683641d3c25d
Chad Jewsbury <cjewsbury@groupon.com>
2014-09-23 22:56:15 -0700

    Playing with views...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @transactions = Transaction.all
+    @budgets = Budget.all
+    @users = User.all
+    @accounts = Account.all
+  end
 end
