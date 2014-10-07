9baedc414bc620318f2c14de84278ff33b0946bb
Chad Jewsbury <cjewsbury@groupon.com>
2014-10-06 22:57:46 -0700

    Adds status and transitions for transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,12 +1,15 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
+  before_action :setup_transaction, only: [:create, :new, :update]
+
   def index
     @transactions = Transaction.all
   end
 
   def create
     @transaction = Transaction.new(transaction_params)
+    @transaction.confirm if @transaction.transaction_date < Time.now
 
     if @transaction.save
       redirect_to @transaction
@@ -17,9 +20,7 @@ class TransactionsController < ApplicationController
 
   def new
     @transaction = Transaction.new
-    @users = User.all
-    @categories = Category.all
-    @accounts = Account.all
+    @transaction.transaction_date = Time.now
   end
 
   def show
@@ -30,10 +31,18 @@ class TransactionsController < ApplicationController
   end
 
   def destroy
+    @transaction.cancel
   end
 
   private
     def transaction_params
       params.require(:transaction).permit(:amount, :user_id, :transaction_date, :category_id, :account_id)
     end
+
+    def setup_transaction
+      @users = User.all
+      @categories = Category.all
+      @accounts = Account.all
+    end
+
 end
